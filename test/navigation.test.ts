2cabea5bf4e0e1404665819fe51af2d40cd837ad
Matsuuu <huhta.matias@gmail.com>
2023-03-06 16:47:17 +0200

    Add navigation tests

--- test/navigation.test.ts
+++ test/navigation.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "@esm-bundle/chai";
 import { clearRenders, getBasicRouterSetup } from "./util";
 
-it("Should render /foo view when window.location.her is set to /foo", () => {
+it("Should render /foo view when window.location.href is set to /foo", () => {
     window.location.href = "/foo";
     clearRenders();
 
@@ -10,4 +10,36 @@ it("Should render /foo view when window.location.her is set to /foo", () => {
 
     const currentView = router.getCurrentView();
     console.log(currentView)
+});
+
+it("Should render dynamic pages with params", () => {
+    window.location.href = "/user/123";
+    clearRenders();
+
+    const router = getBasicRouterSetup();
+    router.start();
+});
+
+it("Should not render dynamic pages with params not mathing the matcher regex", () => {
+    window.location.href = "/user/12bcd";
+    clearRenders();
+
+    const router = getBasicRouterSetup();
+    router.start();
+});
+
+it("Should match the 404 route when route was not found", () => {
+    window.location.href = "/user/12bcd";
+    clearRenders();
+
+    const router = getBasicRouterSetup();
+    router.start();
+});
+
+it("Should redirect correctly", () => {
+    window.location.href = "/user/12bcd";
+    clearRenders();
+
+    const router = getBasicRouterSetup();
+    router.start();
 });
