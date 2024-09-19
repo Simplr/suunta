ab58c39fbdb29eb341a84fc1c7d88c5331024d8f
Matsuuu <huhta.matias@gmail.com>
2024-09-19 15:10:26 +0300

    Fix typings

--- types-generated/suunta.d.ts
+++ types-generated/suunta.d.ts
@@ -1,8 +1,19 @@
+/**
+ * @param { import('./route').Route } route
+ * @param { import('./route').Route } otherRoute
+ */
+export function routeEquals(route: import("./route").Route, otherRoute: import("./route").Route): boolean;
 export class Suunta {
     /**
      * @param {import("./route").SuuntaInitOptions} options
      */
     constructor(options: import("./route").SuuntaInitOptions);
+    /** @type { import("./route").Route | undefined } */
+    currentRoute: import("./route").Route | undefined;
+    /** @type { import("./view").SuuntaView | undefined } */
+    currentView: import("./view").SuuntaView | undefined;
+    /** @type { import("./route").SuuntaTarget | undefined } */
+    currentRenderTarget: import("./route").SuuntaTarget | undefined;
     /** @type { Map<string, import("./route").Route> } */
     routes: Map<string, import("./route").Route>;
     /** @type { Map<RegExp, import("./route").Route> } */
@@ -11,11 +22,19 @@ export class Suunta {
     started: boolean;
     /** @type { import("./route").RouteTransformer<import("./route.js").Route> | undefined } */
     beforeNavigate: import("./route").RouteTransformer<import("./route.js").Route> | undefined;
+    /** @type { import('./route.js').RenderStackEntry[] } */
+    currentNavigationRenderStack: import("./route.js").RenderStackEntry[];
     options: import("./route").SuuntaInitOptions<import("./route").Route>;
+    mapRoutes(): void;
+    /**
+     * @param { import("./route").Route } route
+     */
+    mapRoute(route: import("./route").Route): void;
     /**
      * @returns { Promise<void> }
      */
     start(): Promise<void>;
+    setupListeners(): void;
     /**
      * @param { import("./route").SuuntaTarget } [parentRenderTarget]
      * @param { boolean } [routeIsChildRoute]
@@ -36,7 +55,36 @@ export class Suunta {
      * @returns { import("./route").Route | undefined }
      */
     getRoute(routeQueryObject: import("./route").RouteQueryObject): import("./route").Route | undefined;
+    /**
+     * @returns { import("./route").Route | undefined }
+     */
+    getRouteFromCurrentURL(): import("./route").Route | undefined;
+    /**
+     * @param {import('./route').Route} route
+     * @returns { import('./route').Route[] }
+     */
+    generateStackFromRoute(route: import("./route").Route): import("./route").Route[];
+    /**
+     * @typedef NavigationOptions
+     * @property { boolean } [pushState]
+     * @property { boolean } [isReRender]
+     * @property { MouseEvent | null } [originalEvent]
+     * */
+    /**
+     * @param { import("./route").Route | undefined } route
+     * @param { NavigationOptions } options
+     * @returns {Promise<void>}
+     */
+    navigate(route: import("./route").Route | undefined, { pushState, isReRender, originalEvent }?: {
+        pushState?: boolean | undefined;
+        isReRender?: boolean | undefined;
+        originalEvent?: MouseEvent | null | undefined;
+    }): Promise<void>;
     reNavigate(): Promise<void>;
+    /**
+     * @param {import('./route').RenderStackEntry} stackEntry
+     */
+    emptyStackEntryRenderTarget(stackEntry: import("./route").RenderStackEntry): Promise<void>;
     /**
      * @param {import("./route").Route} route
      */
@@ -77,6 +125,10 @@ export class Suunta {
      */
     refreshView(stackEntry: import("./route").RenderStackEntry, previousEntry?: import("./route").RenderStackEntry | undefined): Promise<void>;
     /**
+     * @param { import('./route').RenderStackEntry } stackEntry
+     * */
+    resetStackEntryRenderTarget(stackEntry: import("./route").RenderStackEntry): void;
+    /**
      * Refreshes all of views and subviews in stack in stack order.
      *
      * Useful for cases where you update a global variable, e.g. localizations and want to apply the changes to all views.
@@ -89,5 +141,4 @@ export class Suunta {
      * @param {unknown} newValue
      */
     triggerOnUpdated(stackEntry: import("./route").RenderStackEntry, propKey: string, oldValue: unknown, newValue: unknown): void;
-    #private;
 }
