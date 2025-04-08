3e0c3f1929c2d18f6e118f6a4e9eb764ae5a33d6
Matsuuu <huhta.matias@gmail.com>
2025-04-08 22:59:24 +0300

    Version bump

--- types/form/index.d.ts
+++ types/form/index.d.ts
@@ -2,6 +2,29 @@
  * @template { Record<string, unknown> } T
  *
  * @param {import('./form').CreateFormOptions<T>} options
+ *
+ * @example
+ *
+ * ```typescript
+ *
+ *  function onSubmit(e: SubmitEvent) {
+ *      console.log(form.data);
+ *   }
+ *
+ * function onFormData(formData: ExampleForm) {
+ *   const result = ExampleFormSchema.safeParse(formData);
+ *      if (result.error) {
+ *           form.reportErrors(result.error.errors);
+ *       }
+ *   }
+ *
+ *   const form = createForm<PreliminaryForm>({
+ *       id: "preliminary-info-form",
+ *       events: ["sl-change"],
+ *       onSubmit,
+ *       onFormData,
+ *   });
+ * ```
  * */
 export function createForm<T extends Record<string, unknown>>({ id, events, onSubmit, onFormData }: import("./form").CreateFormOptions<T>): {
     on: (eventName: string | string[], callback: (event: Event) => any | (() => any)) => void;
