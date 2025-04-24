5be944eebe05d5981769d3585562533b25d41f4d
Matsuuu <huhta.matias@gmail.com>
2025-04-24 21:17:27 +0300

    Fix a lot of things on how array values are handled in form jsonification

--- example/forms/src/views/FormView.ts
+++ example/forms/src/views/FormView.ts
@@ -30,6 +30,8 @@ export function FormView() {
             } else {
                 console.log('No errors');
             }
+
+            console.log(form.data);
         },
 
         onErrorsUpdated: (errors, errorFields, removedErrors) => {
@@ -52,11 +54,29 @@ export function FormView() {
                 <form id="example-form" class="flex flex-col gap-4">
                     <label class="flex flex-col">
                         What is your name?
-                        <input class="border-1" type="text" name="name" placeholder="Matsuuu" />
+                        <input class="border-1" type="text" name="name" value="Matsuuu" />
                         ${when(form.errors.name, () => html`<span class="text-red-500">${form.errors.name}</span>`)}
                     </label>
 
-                    <sl-input label="Other Name" name="otherName"></sl-input>
+                    <sl-input label="Other Name" name="otherName" value="Matsuuu"></sl-input>
+
+                    <sl-input label="Array Value 1" name="arrayValue[]" value="One"></sl-input>
+                    <sl-input label="Array Value 2" name="arrayValue[]" value="Two"></sl-input>
+                    <sl-input label="Array Value 3" name="arrayValue[]" value="Three"></sl-input>
+
+                    <sl-input label="Array object id 1" name="arrayObject[id]" value="Array Object ID 1"></sl-input>
+                    <sl-input
+                        label="Array object name 1"
+                        name="arrayObject[name]"
+                        value="Array Object name 1"
+                    ></sl-input>
+
+                    <sl-input label="Array object id 2" name="arrayObject[id]" value="Array Object ID 2"></sl-input>
+                    <sl-input
+                        label="Array object name 2"
+                        name="arrayObject[name]"
+                        value="Array Object name 2"
+                    ></sl-input>
 
                     <button class="border-2 px-2 py-1" type="submit">Submit</button>
                 </form>
