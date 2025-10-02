0ad0ff56b845a1107d8ae4966e03ce2edf961a59
Matsuuu <huhta.matias@gmail.com>
2025-10-02 20:52:14 +0300

    Update usersView example to actually scramble the results lol

--- example/forms/src/views/UsersView.ts
+++ example/forms/src/views/UsersView.ts
@@ -10,7 +10,6 @@ export function UsersView() {
 
     function addUser() {
         state.users.push({ name: makeid(10) });
-        console.log('Adding user');
     }
 
     function scrambleUser(user) {
@@ -21,8 +20,12 @@ export function UsersView() {
         <div class="flex flex-col items-center justify-center h-full w-full gap-4">
             <h2 class="text-4xl">Users View</h2>
             <button class="border-1 p-2" @click=${addUser}>Add user</button>
-            ${state.users.map(user => html` <p>Name: ${user.name}</p> `)}
-            <button class="border-1 p-2" @click=${scrambleUser}>Scramble</button>
+            ${state.users.map(
+                user => html`
+                    <p>Name: ${user.name}</p>
+                    <button class="border-1 p-2" @click=${() => scrambleUser(user)}>Scramble</button>
+                `,
+            )}
             <a class="underline" href="${router.resolve('HomeView')}">Home View</a>
             <a class="underline" href="${router.resolve('FormView')}">Form View</a>
             <a class="underline" href="${router.resolve('FormComponentView')}">Form component View</a>
